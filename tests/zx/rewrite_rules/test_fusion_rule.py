82e80c799726329e86e63c8a97a93b0349ebf012
Lembe Vardamides Emmanuel <lembe.vardamides@student-cs.fr>
2026-07-15 17:29:52 +0200

    Uncommenting important lines of code.

--- tests/zx/rewrite_rules/test_fusion_rule.py
+++ tests/zx/rewrite_rules/test_fusion_rule.py
@@ -384,9 +384,7 @@ class TestFusionRule(unittest.TestCase):
 
 if __name__ == "__main__":
     # Create output directory for visualizations
-    from mqc3.zx.visualize_base_gates import visualize_before_after, visualize
-    from mqc3.zx.gates import ControlledSumGate, ControlledZGate
-    import matplotlib.pyplot as plt
+    from mqc3.zx.visualize_base_gates import visualize_before_after
 
     # Create rule instance and test objects
     rule_name = "Fusion Rule"
